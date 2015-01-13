e7821198da8eeaa553e86036a02bf4b3f619e4bb
Ben Bowes <benbowes@gmail.com>
2015-01-13 17:17:04 +1100

    Improve comments

--- myAPP.js
+++ myAPP.js
@@ -1,38 +1,75 @@
 
 var myAPP = myAPP || {};
 
-myAPP.Accordion = function () {
-    /* 
-    The Accordion Class is a container for each of the AccordionPanel Class panels.
-    
-    It's 2 methods are:
-    1) resetPanels () - used for unselecting/collapsing every AccordionPanel
-    2) makePanel ( panelElement<HTMLElement>  )  - used for spawning new instances of AccordionPanel
+/* 
 
-    */
-    this.panels = [];
+    This accordion consists of a container ('.accordion') for holding a series of 
+    collapsable panels ('.accordion__panel')
+    Works in IE9+
+
+    -----------------------------------
+    
+    Accordian rules:
+
+    - Panels open and close via a click event on a heading.
+    - Only one panel can be open at a time.
+    - All panels can be closed.
+    - Transitions open in IE10+ and modern browsers
+    
+    -----------------------------------
+
+    HTML Layout:
+
+    <div class="accordion">
+
+        <section class="accordion__panel">
+            <a class="accordion__panel__heading" href="javascript:;">Accordion Panel One</a>
+            <article class="accordion__panel__content">
+                ...
+            </article>
+        </section>
+
+        <section class="accordion__panel">
+            <a class="accordion__panel__heading" href="javascript:;">Accordion Panel One</a>
+            <article class="accordion__panel__content">
+                ...
+            </article>
+        </section>
+
+    </div>
+
+    -----------------------------------
+    
+    Rough HTML translation:
+
+    ('.accordion') = Accordion
+        ('.accordion__panel') = AccordionPanel
+            ('.accordion__panel__heading') = AccordionPanel's heading
+            ('.accordion__panel__content') = AccordionPanel's collapsing area
+
+
+*/
+
+myAPP.Accordion = function () {
+    this.panels = []; // Master list of collapsable panels. Accessible publically e.g myAPP.accordionContainer.panels[0].select();
 };
 
 myAPP.Accordion.prototype = {
 
+    // resetPanels() - used for unselecting/collapsing AccordionPanels
     resetPanels: function () {
         this.panels.forEach( function ( v ) {
             v.unselect();
         });
     },
+    // makePanel(<HTMLElement>) - Spawns a new AccordionPanel and pushes it into the master list of AccordionPanels controlled by Accordian
     makePanel: function ( panelElement ) {
         this.panels.push( new myAPP.AccordionPanel(panelElement, this) );
     }
 };
 
 myAPP.AccordionPanel = function ( el, panelHolder ) {
-    /* 
-    The AccordionPanel Class controls each of the collapsable panels. These are spawned from Accordion Class.
-    
-    It's 2 methods are:
-    1) select () - addClass('active') to this.el ('.accordion__panel') and set this.isSelected=true;
-    2) unselect ()  - removeClass('active') from this.el ('.accordion__panel') and set this.isSelected=false;
-    */
+    // The AccordionPanel Class controls each of the collapsable panels spawned from Accordion Class.
 
     var self = this;
 
@@ -40,15 +77,9 @@ myAPP.AccordionPanel = function ( el, panelHolder ) {
     this.isSelected = false;
     this.panelHolder = panelHolder;
 
-    //  I N I T I A L I Z E   T H I S   I N S T A N C E
-
-    if ( this.panelHolder.panels.length === 0 ){ // select the first panel by default
-        this.select();
-    }
-
     this.el.addEventListener("click", function () {
         
-        if ( self.isSelected ){
+        if (self.isSelected){
             self.unselect(); // already open, presume user wants it closed
         } else {
             self.panelHolder.resetPanels(); // close all panels
@@ -74,24 +105,20 @@ myAPP.AccordionPanel.prototype = {
 
 myAPP.init = function () {
 
-    /*
-    Turn all elements with class '.accordion__panel' into AccordianPanel Class intances. 
+    // Create Accordian instance and turn all elements with class '.accordion__panel' into AccordianPanel Class intances. 
 
-    Each heading when clicked ('.accordion__panel__heading'), will hide all accordion content 
-    elements ('.accordion__panel__content').
-    Then will expand or contract it's sibling accordion content element ('.accordion__panel__content') 
-    depending on it's isSelected value.
-    */
-    
     var accordionPanels = document.querySelectorAll('.accordion__panel'), 
-        i = 0;
+        i,
+        self = this;
 
-    myAPP.accordionContainer = new myAPP.Accordion();
+    this.accordionContainer = new myAPP.Accordion();
 
-    for (i; i < accordionPanels.length; i++) {
-        myAPP.accordionContainer.makePanel(accordionPanels[i]);
+    for (i = 0; i < accordionPanels.length; i++) {
+        self.accordionContainer.makePanel(accordionPanels[i]);
     }
 
+    // select second panel
+    this.accordionContainer.panels[1].select(); // or myAPP.accordionContainer.panels[0].select();
 };
 
 window.onload = function () {
@@ -106,7 +133,7 @@ window.onload = function () {
 
 ------------------------------------------------ */
 
-HTMLElement.prototype.addClass = function( className ){
+HTMLElement.prototype.addClass = function (className) {
     // e.g. el.addClass( 'className' ); 
     if (this.classList){
         this.classList.add(className);
@@ -115,7 +142,7 @@ HTMLElement.prototype.addClass = function( className ){
     }
 }
 
-HTMLElement.prototype.removeClass = function( className ){
+HTMLElement.prototype.removeClass = function (className) {
     // e.g. el.removeClass( 'className' ); 
     if (this.classList){
       this.classList.remove(className);
